03337a751387ac5bb17a0b5750f84adc97157ecc
anttinym <nyman.antti@gmail.com>
2018-11-27 18:30:12 +0200

    setup pm2 for deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'Aalto-api',
+    script: './server.js',
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-222-0-63.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/post-server-key.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:TheRealSense/aalto-app-rest-api.git',
+      path: '/home/ubuntu/server',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js',
+    },
+  },
+}
